7dedd6e07afc52de407cb7fa040a6145ab3dcfe5
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-08-11 10:06:01 +0700

    Add project files.

---
+++ SQLServer-EF6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SQLServer_EF6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
