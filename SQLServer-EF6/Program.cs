da309101ac0f5aa41789d64bc57c477bc999635a
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-08-18 11:31:01 +0700

    Minor changes

--- SQLServer-EF6/Program.cs
+++ SQLServer-EF6/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 // setup DBContext
 builder.Services.AddDbContext<GeneralDBContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("cnnstr")));
